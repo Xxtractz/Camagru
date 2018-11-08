d8cf17e1f69ed01a586dd472b415583fe541d8a0
Musa Martin BALOYI <mbaloyi@e5r2p5.wethinkcode.co.za>
2018-11-08 13:13:39 +0200

    working on front end

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,3 +1,2 @@
 <?php $this->start('body'); ?>
-<h1>This is Camagru body </h1>
 <?php $this->end(); ?>
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -9,11 +9,29 @@
     <title><?= $this->siteTitle(); ?></title>
     <link rel="stylesheet" type="text/css" media="screen" href="<?=PROOT?>/css/main.css" />
     <link rel="stylesheet" type="text/css" media="screen" href="<?=PROOT?>/css/layout.css" />
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" 
+        integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
     <script src="main.js"></script>
 
     <?= $this->content('head'); ?>
 </head>
-<body>
-    <?= $this->content('body'); ?>
+<body class="body">
+    <div class = "navbar">
+        <a class="fas fa-camera-retro fa-2x"></a>
+        <a href="#" class="right fas fa-sign-in-alt "> login</a>
+        <a href="#" class="right fas fa-user-plus" > Sign_up</a>
+        <a href="#" class="right fas fa-cogs"> Settings</a>
+        <a href="#" class="right fas fa-sign-out-alt "> Logout</a>
+        <a href="#" class="right fas fa-user-circle"> Profile</a>
+        <a href="#" class="right fas fa-home"> Home</a>
+    </div>
+
+    <div class= "content">
+        <?= $this->content('body'); ?>
+    </div>
+
+    <div class="footer">
+    <p>All Rights Reserved. &copy; Camagru | Wethinkcode_</p>
+    </div>
 </body>
 </html>
